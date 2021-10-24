3ff409b81005358faad7f5c94b0a4fa7365121b3
Pefacel <pefacel@gmail.com>
2021-10-24 06:31:35 -0300

    Restrict actions for unauthorized users

--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -37,4 +37,30 @@ class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
     assert_response :no_content
   end
 
+  test "should update user" do
+      patch api_v1_user_url(@user),
+      params: { user: { email: @user.email } },
+      headers: { Authorization: JsonWebToken.encode(user_id:@user.id) },as: :json
+      assert_response :success
+  end
+
+  test "should forbid update user" do
+    patch api_v1_user_url(@user), params: { user: { email:@user.email } }, as: :json
+    assert_response :forbidden
+  end
+
+  test "should destroy user" do
+    assert_difference('User.count', -1) do
+      delete api_v1_user_url(@user), headers: { Authorization:JsonWebToken.encode(user_id: @user.id) }, as: :json
+    end
+    assert_response :no_content
+  end
+
+  test "should forbid destroy user" do
+    assert_no_difference('User.count') do
+      delete api_v1_user_url(@user), as: :json
+    end
+    assert_response :forbidden
+  end
+  
 end
