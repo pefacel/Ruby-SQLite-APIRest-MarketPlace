0ea9ed779ec43ac8fb6ced443375e25640f42908
Pefacel <pefacel@gmail.com>
2021-10-24 04:18:15 -0300

    Set the versioning namespaces for API

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   #mapea en controllers la carpeta api y le asigna, como tipo de archivo, JSON
   namespace :api, defaults: { format: :json } do
-
+    namespace :v1 do
+      # We are going to list our resources here
+      end
   end
 end
