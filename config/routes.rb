b6485ff48ea88948b58102ed87512e802e4b7f05
Pefacel <pefacel@gmail.com>
2021-10-24 04:48:29 -0300

    Adds show action to the users controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   #mapea en controllers la carpeta api y le asigna, como tipo de archivo, JSON
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      # We are going to list our resources here
-      end
+        resources :users, only: [:show]      end
   end
 end
