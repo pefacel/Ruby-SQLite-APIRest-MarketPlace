07051661e366c997dce5ab009cf260f052cd8b4b
Pefacel <pefacel@gmail.com>
2021-10-24 05:16:29 -0300

    Adds update action the users controller

--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -4,6 +4,7 @@ class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
   setup do
     @user = users(:one)
   end
+
   test "should show user" do
     get api_v1_user_url(@user), as: :json
     assert_response :success
@@ -11,11 +12,22 @@ class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
     json_response = JSON.parse(self.response.body)
     assert_equal @user.email, json_response['email']
     end
-    test "should not create user with taken email" do
+
+  test "should not create user with taken email" do
       assert_no_difference('User.count') do
         post api_v1_users_url, params: { user: { email: @user.email, password: '123456' } }, as: :json
       end
       assert_response :unprocessable_entity
     end
+
+    test "should update user" do
+      patch api_v1_user_url(@user), params: { user: { email:@user.email, password: '123456' } }, as: :json
+      assert_response :success
+      end
+      test "should not update user when invalid params are sent" do
+      patch api_v1_user_url(@user), params: { user: { email:'bad_email', password: '123456' } }, as: :json
+      assert_response :unprocessable_entity
+      end
+
 
 end
