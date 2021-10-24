e327c479ff34cccf1a78eda367280c56ccd87ddb
Pefacel <pefacel@gmail.com>
2021-10-24 05:04:58 -0300

    Adds the user create endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  #mapea en controllers la carpeta api y le asigna, como tipo de archivo, JSON
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-        resources :users, only: [:show]      end
+      resources :users, only: %i[show create]
+        end
   end
 end
